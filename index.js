31e7e330c6f051a896578619190c2b6b8701132f
Skandha S <ssss1@LAPTOP-TDR3HNJ7.(none)>
2024-08-07 01:58:04 +0530

    working after add js and css

---
+++ index.js
@@ -0,0 +1,9 @@
+
+console.log("HEllo")
+
+
+document.querySelector(".button1").addEventListener("click",function(){
+    document.querySelector(".button1").classList.toggle("button1-effects")
+    document.querySelector(".heading").classList.toggle("heading-effects")
+
+})
